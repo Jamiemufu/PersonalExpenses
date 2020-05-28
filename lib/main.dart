4c4b6f01be60faa8a96c2ffe5167dfe481721dd9
Jamie <jamie@supercooldesign.co.uk>
2020-05-28 13:43:01 +0100

    Added transaction class and set up first widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,15 +15,47 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Monitor',
+        amount: 142.99,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: 't2',
+        title: 'Monitor Stand',
+        amount:62.49,
+        date: DateTime.now()
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        // column contains children widgets
+        body: Column(
+            // column and rows are like flexbex - main axis and crossaxis
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              // you can wrap widgets in a parent such as container
+              // you can use this to set certain properties on that specific widget
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("CHART!"),
+                  elevation: 5,
+                ),
+              ),
+              Card(
+                color: Colors.red,
+                child: Text("List of TX"),
+              ),
+            ]));
   }
 }
