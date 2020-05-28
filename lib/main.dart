8ae95b4b1c98e9fd8c5feedfaff7519630ef542c
Jamie <jamie@supercooldesign.co.uk>
2020-05-28 17:25:46 +0100

    Formatting

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,11 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-        id: 't1', title: 'Monitor', amount: 142.99, date: DateTime.now()),
+        id: 't1', title: 'Monitor', amount: 142.99, date: DateTime.now()
+        ),
     Transaction(
-        id: 't2', title: 'Monitor Stand', amount: 62.49, date: DateTime.now()),
+        id: 't2', title: 'Monitor Stand', amount: 62.49, date: DateTime.now()
+        ),
   ];
 
   @override
@@ -39,11 +41,7 @@ class MyHomePage extends StatelessWidget {
               // you can use this to set certain properties on that specific widget
               Container(
                 width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text("CHART!"),
-                  elevation: 5,
-                ),
+                child: Card(color: Colors.blue,child: Text("CHART!"),elevation: 5,),
               ),
               Card(
                 child: Container(
@@ -51,52 +49,30 @@ class MyHomePage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: <Widget>[
-                      // start of container
                       Container(
-                        child: TextField(
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                          ),
+                        child: TextField(style: TextStyle(color: Colors.black, fontSize: 14),
                           cursorColor: Colors.black,
                           decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(0),
-                            enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.purple,
-                            )),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.purple)),
                             labelText: 'Title',
-                            labelStyle: TextStyle(
-                              color: Colors.purple,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 21,
-                            ),
+                            contentPadding: EdgeInsets.all(0),
+                            enabledBorder: UnderlineInputBorder( borderSide: BorderSide(color: Colors.purple,)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.purple)),
+                            labelStyle: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold,fontSize: 21,),
                           ),
                         ),
                       ),
                       Container(
                         margin: EdgeInsets.only(top: 15),
-                        child: TextField(
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                          ),
+                        child: TextField(style: TextStyle(color: Colors.black,fontSize: 14,),
                           cursorColor: Colors.purple,
                           decoration: InputDecoration(
-                              prefixText: "£",
-                              contentPadding: EdgeInsets.all(0),
-                              enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.purple)),
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.purple)),
-                              labelText: 'Amount',
-                              labelStyle: TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 21,
-                              )),
+                            labelText: 'Amount',
+                            prefixText: "£",
+                            contentPadding: EdgeInsets.all(0),
+                            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.purple)),
+                            focusedBorder: UnderlineInputBorder( borderSide: BorderSide(color: Colors.purple)),
+                            labelStyle: TextStyle(color: Colors.purple,fontWeight: FontWeight.bold,fontSize: 21,)
+                          ),
                         ),
                       ),
                       FlatButton(
@@ -113,53 +89,40 @@ class MyHomePage extends StatelessWidget {
                 children: transactions.map((tx) {
                   return Card(
                       child: Row(
-                    // inside this row we have a container and a column
-                    children: <Widget>[
-                      Container(
-                        alignment: Alignment.center,
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(5),
-                        width: 100,
-                        child: Text(
-                          '£${tx.amount}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.purple,
+                      // inside this row we have a container and a column
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                          decoration:BoxDecoration( border: Border.all(color: Colors.purple, width: 2,)),
+                          padding: EdgeInsets.all(5),
+                          width: 100,
+                          child: Text(
+                            '£${tx.amount}',
+                            style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold, color: Colors.purple),
                           ),
                         ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),
                             ),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                              fontSize: 11,
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ));
+                            Text(
+                              DateFormat.yMMMd().format(tx.date),
+                              style: TextStyle(fontSize: 11,color: Colors.grey),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  );
                 }).toList(),
               )
-            ]));
+           ]
+        )
+      );
   }
 }
